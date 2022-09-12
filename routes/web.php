bceaaacbf631b666f0e9e9eca086e2770deb38a2
Mamadou MARÉGA <hamshakour93@gmail.com>
2022-09-13 00:27:21 +0200

    Some configurations

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('welcome');
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::get('/contact', [PostController::class, 'contact'])->name('contact');
