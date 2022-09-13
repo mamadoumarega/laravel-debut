7dd9062e4e461c0b10c3e179f0a981e76111f3ad
Mamadou MARÉGA <hamshakour93@gmail.com>
2022-09-13 23:57:14 +0200

    Display data

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -2,5 +2,11 @@
 
 @section('content')
     <h1>Liste des articles</h1>
-    <h2>{{ $title }}</h2>
+    @if ($posts->count() > 0)
+        @foreach($posts as $post)
+            <h2><a href="{{ route('posts.show', ['id' => $post->id]) }}">{{ $post->title }}</a></h2>
+        @endforeach
+    @else
+        <span>Aucun post en base de données</span>
+    @endif
 @endsection
