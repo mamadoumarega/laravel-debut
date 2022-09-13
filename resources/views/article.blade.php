44ed099d2cfceef29cbdc5f036e755dcc4d2c6f4
Mamadou MARÉGA <hamshakour93@gmail.com>
2022-09-14 00:49:46 +0200

    Display comments

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,4 +3,12 @@
 
 @section('content')
     <h1>{{ $post->content }}</h1>
+
+    <hr>
+
+    @forelse($post->comments as $comment)
+        <div>{{ $comment->content }} crée le {{ $comment->created_at->format('d/m/y') }}</div>
+    @empty
+        <sapn>Aucun commentaire pour ce post</sapn>
+    @endforelse
 @endsection
