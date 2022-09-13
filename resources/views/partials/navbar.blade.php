520337886897cc8d90087b73e234de9444a96dd7
Mamadou MARÉGA <hamshakour93@gmail.com>
2022-09-13 23:57:50 +0200

    Styling

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,5 @@
-<ul>
-    <li><a href="{{ route('welcome') }}">L'accueil</a></li>
+<ul class=" bg-blue-400 space-x-4">
+    <li class="mr-2"><a href="{{ route('welcome') }}">Accueil</a></li>
+    <li class="mr-2"><a href="{{ route('posts.create') }}">Création</a></li>
     <li><a href="{{ route('contact') }}">Page de contact</a></li>
 </ul>
